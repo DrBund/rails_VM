393e2f6d19ae7ffce39f8270bafc128567a0f8e9
Steve Lammers <StevenLammers12@gmail.com>
2016-06-16 07:18:55 -0600

    Initial commit working

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'rails_VM'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures rails_VM'
+long_description 'Installs/Configures rails_VM'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: rails_VM
+# Recipe:: default
+#
+# Copyright (C) 2016 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
